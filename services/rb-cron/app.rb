156cad322abcb8bff17c39910112873104ec3ab6
S. M. Mohiuddin Khan Shiam <mohiuddin.khan.shiam@gmail.com>
2025-06-22 20:44:47 +0600

    update 3.0

--- services/rb-cron/app.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-require 'date'
-
-# Simple cron-like task to rotate a season every hour
-seasons = %w[spring summer autumn winter]
-index = DateTime.now.hour % seasons.length
-puts({ season: seasons[index], at: Time.now }.to_json)
